577ff1bc029e2be94e3701d95b5d9f9073185330
aurefsec <auref.contact@protonmail.com>
2026-05-04 06:37:33 +0200

    data and checksum of icmp packet

--- src/icmp_loop.c
+++ src/icmp_loop.c
@@ -1,25 +1,42 @@
 #include "ft_ping.h"
 
-int  icmp_loop(t_ping* data);
+int create_packet(t_icmp_header* packet)
 {
-  // Create ICMP packet.
-  t_icmp_header   packet;
-  struct timeval  time;
-  memset(&packet, 0, sizeof(packet));
-  memset(&time, 0, sizeof(time));
+  packet.type = ICMP_ECHO; // Type value : 8, type return : 0 (ICMP_ECHOREPLY).
+  packet.code = 0;
+  packet.identifier = (uint16_t)getpid();
+  packet.sequencer = 1; // Index for router jumps.
 
+  // Copy actual time in data of icmp packet.
+  struct timeval  time;
+  memset(&time, 0, sizeof(time));
   if (gettimeofday(&time, NULL) == -1)
   {
     fprintf(stderr, "error gettimeofday : %s\n", strerror(errno)); // NEED TO CHECK THE REAL ERROR !
     return 0;
   }
+  memcpy(packet.data, &time, sizeof(time));
 
-  packet.type = ICMP_ECHO; // Send value : 8, send return : 0 (ICMP_ECHOREPLY).
-  packet.code = 0;
-  packet.identifier = (uint16_t)getpid();
-  packet.sequencer = 1; // Index for router jumps.
-  // packet.data = ?
-  // packet.checksum = ?
+  // Checksum calcul.
+  uint32_t sum = 0; // uint32_t to add each word of packet + carry.
+  uint16_t  *word = (uint16_t*)packet; // Each word = 2 bytes.
+  for (int i = 0; i < 32; i++)
+    sum += word[i];
+  packet.checksum = (sum >> 16) + (uint16_t)sum; // Get last 16 bytes and add to first 16 bytes (Carry).
+  packet.checksum = ~packet.checksum; // ~ to reverse all bits.
+
+  return 1;
+}
+
+int icmp_loop(t_ping* data);
+{
+  // Create ICMP packet.
+  t_icmp_header   packet;
+  memset(&packet, 0, sizeof(packet));
+
+  if (!create_packet(&packet))
+    return 0;
+  // Loop send/recv.
 
   return 1;
 }
